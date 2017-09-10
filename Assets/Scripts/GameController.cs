b47f75ae7682779ce1761d44770fe88a3a56ebdc
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-09-09 21:22:18 -0400

    basic falling objects

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject fallingObject;
+	public Vector3 spawnValues;
+	public float spawnInterval;
+
+	void Start () {
+		StartCoroutine (SpawnFallingObjects ());
+	}
+
+	IEnumerator SpawnFallingObjects () {
+		while (true)
+		{
+			Vector3 spawnPosition = new Vector3 (Random.Range (0, spawnValues.x), spawnValues.y, Random.Range (0, spawnValues.z));
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (fallingObject, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (spawnInterval);
+		}
+	}
+}
