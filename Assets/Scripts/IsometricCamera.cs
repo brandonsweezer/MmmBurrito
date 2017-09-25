61f90ff3caeea9d72a220b65ca695ccd0d7a25f6
Noah Sterling <nsterling4@dhcp-rhodes-667.eduroam.cornell.edu>
2017-09-25 11:01:25 -0400

    isoCam

---
+++ Assets/Scripts/IsometricCamera.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCamera : MonoBehaviour {
+
+
+	private const float Y_ANGLE_MIN = 0.0f;
+	private const float Y_ANGLE_MAX = 50.0f;
+
+	public GameObject gameControllerObject; 
+	public float restDistance; 
+	public float restHeight; 
+	public float moveDistance;
+	public float moveHeight; 
+	public float speed;
+
+	public Transform camTransform;
+	private Camera cam;
+
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+
+	// Use this for initialization
+	private void Start () {
+		camTransform = transform;
+		cam = Camera.main;
+	}
+
+
+
+
+
+
+
+	// Update is called once per frame
+	private void Update () {
+		currentX += Input.GetAxis ("Horizontal");
+		currentY += Input.GetAxis ("Vertical");
+
+		currentY = Mathf.Clamp (currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+	}
+
+	private void LateUpdate () {
+		GameObject burrito = gameControllerObject.GetComponent<GameController> ().player;
+		Transform target = burrito.transform;
+		if (!burrito.GetComponent<MovementController> ().getMovement()) {
+			Vector3 dir = new Vector3 (0, restHeight, -restDistance);
+			Quaternion rotation = Quaternion.Euler (0, 0, 0); //currentY? X!, z
+			camTransform.position = target.position + rotation * dir; 
+			camTransform.LookAt (target.position);
+		}
+		else{
+			Vector3 dir = new Vector3 (0, moveHeight, -moveDistance);
+			Quaternion rotation = Quaternion.Euler (0, 0, 0);
+			camTransform.position = target.position + rotation * dir; 
+			camTransform.LookAt (target.position);
+		}
+	}
+}
