4ca5f1bf8b3e4360fa7ca077102583bae5a46dfe
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-13 00:16:38 -0400

    objects now spawn on tile center

--- Assets/Scripts/Misc Scripts/ObjectSpawn.cs
+++ Assets/Scripts/Misc Scripts/ObjectSpawn.cs
@@ -6,7 +6,8 @@ public class ObjectSpawn : MonoBehaviour {
 	public GameObject[] fallingObjectList;
 	public float spawnInterval;
 
-	private static float spawnHeight = 10f;
+	private static float spawnYOffset = 10f;
+	private static float maxSpawnHeight = 200f;
 
 	private float[] spawnRangeX;
 	private float[] spawnRangeZ;
@@ -21,11 +22,27 @@ public class ObjectSpawn : MonoBehaviour {
 
 	IEnumerator SpawnFallingObjects () {
 		while (true) {
+
+			// Determine Position
+			Vector3 spawnPosition = new Vector3 (Random.Range (spawnRangeX[0], spawnRangeX[1]), maxSpawnHeight, Random.Range (spawnRangeZ[0], spawnRangeZ[1]));
+			// Snap to tiled position
+			spawnPosition.x = Mathf.Round(spawnPosition.x / TiledFloor.tileWidth) * TiledFloor.tileWidth;
+			spawnPosition.z = Mathf.Round(spawnPosition.x / TiledFloor.tileHeight) * TiledFloor.tileHeight;
+			// Offset position a certain distance above ground below that position
+			RaycastHit hit;
+			bool raycastResult = Physics.Raycast (spawnPosition, Vector3.down, out hit, maxSpawnHeight);
+			if (!raycastResult) {
+				// Debug.LogError ("Oops! An object spawn region is hovering over the void! Spawning object at height "+maxSpawnHeight);
+			} else {
+				spawnPosition.y = hit.point.y + spawnYOffset;
+			}
+
+			// Spawn the object
 			GameObject objectToSpawn = fallingObjectList[Random.Range(0, fallingObjectList.Length)];
-			Vector3 spawnPosition = new Vector3 (Random.Range (spawnRangeX[0], spawnRangeX[1]), spawnHeight, Random.Range (spawnRangeZ[0], spawnRangeZ[1]));
 			Quaternion spawnRotation = Quaternion.identity;
 			GameObject obj = Instantiate (objectToSpawn, spawnPosition, spawnRotation) as GameObject;
-            LoggingManager.instance.RecordEvent(1, obj.name+ "Spawned");
+
+			LoggingManager.instance.RecordEvent(1, obj.name+ "Spawned");
 			yield return new WaitForSeconds (spawnInterval);
 		}
 	}
