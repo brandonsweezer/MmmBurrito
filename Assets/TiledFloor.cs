a06108784fa634b085394b98b424f988e0547262
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-12 23:59:31 -0400

    basic tiling (see lvl 4)

---
+++ Assets/TiledFloor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TiledFloor : MonoBehaviour {
+
+	public static float tileWidth = 2f;
+	public static float tileHeight = tileWidth;
+
+	public Renderer rend;
+
+	void Start() {
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update() {
+		float tileScaleX = rend.bounds.size.x / tileWidth;
+		float tileScaleY = rend.bounds.size.z / tileHeight;
+		rend.material.mainTextureScale = new Vector2(tileScaleX, tileScaleY);
+	}
+
+}
