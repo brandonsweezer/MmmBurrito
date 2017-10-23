dff500ca502c6c7e9613099086b457fcbbb08a8e
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-23 17:53:18 -0400

    moved trashing to TrashingController and position logging to LoggingCalls

---
+++ Assets/LoggingCalls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoggingCalls : MonoBehaviour {
+
+	private static float positionLogCooldown = 1f;
+	private static float lastPositionLog;
+	
+	// Update is called once per frame
+	void Update () {
+		LogPlayerPosition ();
+	}
+
+	// Logs the player's position if a player exists and enough time has elapsed since the last position logging.
+	void LogPlayerPosition () {
+		if (GameController.instance.player == null) {
+			return;
+		}
+		if (Time.time - lastPositionLog > positionLogCooldown) {
+			lastPositionLog = Time.time;
+			Debug.Log ("logged pos");
+			string positionLogMessage = "Coordinates: " + GameController.instance.player.transform.position.x + 
+				", " + GameController.instance.player.transform.position.z;
+			LoggingManager.instance.RecordEvent (0, positionLogMessage);
+		}
+	}
+}
---
+++ Assets/TrashingController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashingController : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			LoggingManager.instance.RecordEvent(3, "Trashed ingredients with T: " + GameController.instance.player.GetComponent<ObjectCatcher>().getIngredients().ToString());
+			GameController.instance.player.GetComponent<ObjectCatcher>().getIngredients().Empty();
+			OrderUI.instance.ResetAfterDeath();
+		}
+	}
+}
