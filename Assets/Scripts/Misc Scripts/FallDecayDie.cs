3edd72a65ac9cb4efdb4031614eb12c466c024db
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-27 05:17:52 -0400

    fixed a bug where we could have null indicator for hazards

--- Assets/Scripts/Misc Scripts/FallDecayDie.cs
+++ Assets/Scripts/Misc Scripts/FallDecayDie.cs
@@ -46,11 +46,13 @@ public class FallDecayDie : MonoBehaviour {
 		if (slowFalling) {
 			float fallSpeed = slowFallSpeed;
 			// Increase the fall speed if the burrito is on the indicator and barely moving.
-			Vector3 playerPos = GameController.instance.player.transform.position;
-			Vector3 indicatorPos = GetComponent<IngredientIndicator> ().indicator.transform.position;
-			float playerSpeed = GameController.instance.player.GetComponent<Rigidbody> ().velocity.magnitude;
-			if (Vector3.Distance(playerPos, indicatorPos) <= TiledFloor.tileHeight * 1.414f && playerSpeed <= playerMaxSpeedForFastFall) {
-				fallSpeed = fastFallSpeed;
+			if (GetComponent<IngredientIndicator> () != null && GetComponent<IngredientIndicator> ().indicator != null) {
+				Vector3 playerPos = GameController.instance.player.transform.position;
+				Vector3 indicatorPos = GetComponent<IngredientIndicator> ().indicator.transform.position;
+				float playerSpeed = GameController.instance.player.GetComponent<Rigidbody> ().velocity.magnitude;
+				if (Vector3.Distance (playerPos, indicatorPos) <= TiledFloor.tileHeight * 1.414f && playerSpeed <= playerMaxSpeedForFastFall) {
+					fallSpeed = fastFallSpeed;
+				}
 			}
 
 			rb.velocity = new Vector3 (rb.velocity.x, -fallSpeed, rb.velocity.z);
