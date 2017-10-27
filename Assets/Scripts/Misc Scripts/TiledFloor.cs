95970303d2e122b3439fd67a1430540005ae2183
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-26 21:19:42 -0400

    floor now gets tiled in editor too

--- Assets/Scripts/Misc Scripts/TiledFloor.cs
+++ Assets/Scripts/Misc Scripts/TiledFloor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class TiledFloor : MonoBehaviour {
 
 	public static float tileWidth = 2f;
@@ -14,15 +15,18 @@ public class TiledFloor : MonoBehaviour {
 	}
 
 	void Update() {
+		var tempMaterial = new Material(rend.sharedMaterial);
+		rend.sharedMaterial = tempMaterial;
+
 		float tileScaleX = rend.bounds.size.x / tileWidth;
 		float tileScaleY = rend.bounds.size.z / tileHeight;
-		rend.material.mainTextureScale = new Vector2(tileScaleX, tileScaleY);
+		rend.sharedMaterial.mainTextureScale = new Vector2(tileScaleX, tileScaleY);
 
 		float xEdge = transform.position.x + rend.bounds.size.x/2;
 		float zEdge = transform.position.z + rend.bounds.size.z/2;
 		float tileOffsetX = (- ((xEdge - (tileWidth / 2)) % tileWidth)) / tileWidth;
 		float tileOffsetY = (- ((zEdge - (tileHeight / 2)) % tileHeight)) / tileHeight;
-		rend.material.mainTextureOffset = new Vector2 (tileOffsetX, tileOffsetY);
+		rend.sharedMaterial.mainTextureOffset = new Vector2 (tileOffsetX, tileOffsetY);
 	}
 
 }
