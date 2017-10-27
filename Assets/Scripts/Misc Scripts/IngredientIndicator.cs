80ffcf35ec6fcdf1106db140293056a69f800184
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-10-27 04:52:53 -0400

    ingredient indicators now scale up as the ingredient drops

--- Assets/Scripts/Misc Scripts/IngredientIndicator.cs
+++ Assets/Scripts/Misc Scripts/IngredientIndicator.cs
@@ -7,6 +7,10 @@ public class IngredientIndicator : MonoBehaviour {
 	public GameObject indicatorPrefab;
 	public Texture2D image;
 
+	private static Vector3 startingScale = new Vector3 (0.3f, 0.3f, 0.3f);
+	private static Vector3 endingScale = new Vector3 (1f, 1f, 1f);
+	private float originalDistanceFromIndicator;
+
 	private GameObject indicator;
 
 	// Need this in awake for the same reason we need it in awake in the FallDecayDie script
@@ -14,12 +18,24 @@ public class IngredientIndicator : MonoBehaviour {
 		RaycastHit hit;
 		bool raycast = ObjectSpawn.RaycastUntilTerrain(transform.position, Vector3.down, out hit);
 		if (raycast) {
-			indicator = Instantiate (indicatorPrefab, hit.point, Quaternion.AngleAxis (180, Vector3.up)) as GameObject;
-			indicator.GetComponent<Renderer> ().material.mainTexture = image;
+			indicator = Instantiate (indicatorPrefab, hit.point, Quaternion.identity) as GameObject;
+			indicator.transform.GetChild(0).GetComponent<Renderer> ().material.mainTexture = image;
+			indicator.transform.localScale = startingScale;
+			originalDistanceFromIndicator = CalculateDistanceToIndicator ();
+		}
+	}
+
+	void Update() {
+		if (indicator != null) {
+			indicator.transform.localScale = Vector3.Lerp (endingScale, startingScale, CalculateDistanceToIndicator () / originalDistanceFromIndicator);
 		}
 	}
 	
 	public void DestroyIndicator () {
 		Destroy (indicator);
+	}
+
+	float CalculateDistanceToIndicator() {
+		return Vector3.Distance (transform.position, indicator.transform.position);
 	}
 }
