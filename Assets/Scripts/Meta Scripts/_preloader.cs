9e0e7dcf30228da5e078a57485020e4035d2abd2
Joshua Chan <jdc345@cornell.edu>
2017-10-28 14:36:45 -0400

    fixed logging

--- Assets/Scripts/Meta Scripts/_preloader.cs
+++ Assets/Scripts/Meta Scripts/_preloader.cs
@@ -7,7 +7,7 @@ public class _preloader : MonoBehaviour {
 
 	void Awake () {
 		SceneManager.LoadScene ("LevelSelection");
-        LoggingManager.instance.Initialize(094, 2, true);
+        LoggingManager.instance.Initialize(094, 2, false);
         LoggingManager.instance.RecordPageLoad();
 
         /* GAME EVENT IDS
