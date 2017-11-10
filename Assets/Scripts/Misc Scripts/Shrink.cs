33c13de927a51d013ba1b8f20c30ccd712f3e819
Joshua Chan <jdc345@cornell.edu>
2017-11-09 20:22:48 -0500

    cleaned up shrinking code

--- Assets/Scripts/Misc Scripts/Shrink.cs
+++ Assets/Scripts/Misc Scripts/Shrink.cs
@@ -4,21 +4,27 @@ using System.Collections;
 public class Shrink : MonoBehaviour
 {
     private bool shrinking;
+    private bool gone;
 
     // Use this for initialization
     void Awake()
     {
         shrinking = false;
+        gone = false;
     }
 
     private void Update()
     {
         if (shrinking)
         {
+            if (!gone)
+            {
+                GameController.instance.objects.RemoveAt(GameController.instance.objects.IndexOf(gameObject));
+                gone = true;
+            }
             transform.localScale -= Vector3.one * Time.deltaTime*0.7f;
             if(this.transform.localScale.x < 0.3f)
             {
-                GameController.instance.objects.RemoveAt(GameController.instance.objects.IndexOf(gameObject));
                 Destroy(gameObject);
             }
         }
