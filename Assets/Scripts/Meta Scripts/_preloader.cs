e09e9330436a8c60503d2d5e9669a12eb7c9b0d4
Joshua Chan <jdc345@cornell.edu>
2017-11-15 10:37:07 -0500

    logging ON

--- Assets/Scripts/Meta Scripts/_preloader.cs
+++ Assets/Scripts/Meta Scripts/_preloader.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class _preloader : MonoBehaviour {
 
 	void Awake () {
-        LoggingManager.instance.Initialize(094, 3, true);
+        LoggingManager.instance.Initialize(094, 3, false);
         GameController.instance.ABValue = LoggingManager.instance.assignABTestValue(Random.Range(1, 3));
         LoggingManager.instance.RecordPageLoad();
         LoggingManager.instance.RecordABTestValue();
