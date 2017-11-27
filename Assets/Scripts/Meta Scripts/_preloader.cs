be45f3fa4d418f965f6038f37e6d2461476314f8
Joshua Chan <jdc345@cornell.edu>
2017-11-27 17:09:23 -0500

    version 4, logging off

--- Assets/Scripts/Meta Scripts/_preloader.cs
+++ Assets/Scripts/Meta Scripts/_preloader.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class _preloader : MonoBehaviour {
 
 	void Awake () {
-        LoggingManager.instance.Initialize(094, 3, false);
+        //version 4 is KONGREGATE
+        LoggingManager.instance.Initialize(094, 4, true);
         LoggingManager.instance.RecordPageLoad();
         GameController.instance.ABValue = LoggingManager.instance.assignABTestValue(Random.Range(1, 3));
         LoggingManager.instance.RecordABTestValue();
