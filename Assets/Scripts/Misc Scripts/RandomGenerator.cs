364df1f329583e609978fb38163ae65eec4990a4
Noah Sterling <nsterling4@Noah-Sterlings-MacBook-Pro.local>
2017-11-27 11:15:39 -0500

    level complete screen ann and instructions screen

---
+++ Assets/Scripts/Misc Scripts/RandomGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class RandomGenerator
+{
+	private static readonly Random random = new Random();
+
+	public static double RandomNumberBetween(double minValue, double maxValue)
+	{
+		var next = random.NextDouble();
+
+		return minValue + (next * (maxValue - minValue));
+	}
+}
+
