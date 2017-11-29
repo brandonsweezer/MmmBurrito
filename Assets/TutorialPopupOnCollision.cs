0c2c377384d21ce023622c25e8878d3642984886
Nicolas Kuhn de Chizelle <nk436@cornell.edu>
2017-11-29 18:29:06 -0500

    basic tutorial popup coded in

---
+++ Assets/TutorialPopupOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialPopupOnCollision : MonoBehaviour {
+
+	public string textToDisplay;
+	public GameObject UIPopupPrefab;
+
+	private GameObject popupObject;
+
+	void Start() {
+		popupObject = Instantiate (UIPopupPrefab) as GameObject;
+		popupObject.transform.GetChild(0).GetComponent<TextMeshPro> ().text = textToDisplay;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.collider.tag == "Player") {
+			popupObject.GetComponent<TutorialPopupManager> ().Show ();
+		}
+	}
+
+	void OnCollisionExit(Collision collision) {
+		if (collision.collider.tag == "Player") {
+			popupObject.GetComponent<TutorialPopupManager> ().Hide ();
+		}
+	}
+
+}
